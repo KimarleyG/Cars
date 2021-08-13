85daf0eb5db6cd2cebcd0076eaaabd54522237c6
KimarleyG <kimarleygrant@gmail.com>
2021-08-13 15:48:59 -0700

    learned how to add stuff to gitignore

---
+++ pages/carpics.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1> just a try something </h1>
+            <picture>
+                <img src="https://cdn.motor1.com/images/mgl/ZobwZ/s1/next-gen-honda-civic-type-renderings.jpg " width="900" height="600">
+            </picture>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div> <title> All Cars </title> 
+    <h1> Car Names </h1> 
+    <h2> Honda </h2> <br>
+    <h2> Toyota</h2> <br>
+    <h2> Nissan </h2> <br>
+    <h2> Suzuki </h2> <br>
+    <body>
+    <p> So much more to learn but in time ill be better</p>
+    </body>
+    
+    </div>
+</template>
