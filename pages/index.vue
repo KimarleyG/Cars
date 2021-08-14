07da3c382b325a945909b9165421879a1733d09c
KimarleyG <kimarleygrant@gmail.com>
2021-08-13 19:52:33 -0700

    made some key changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,16 @@
 <template>
     <div> <title> All Cars </title> 
     <h1> Car Names </h1> 
+
+    <body>
     <h2> Honda </h2> <br>
     <h2> Toyota</h2> <br>
     <h2> Nissan </h2> <br>
     <h2> Suzuki </h2> <br>
-    <body>
-    <p> So much more to learn but in time ill be better</p>
-    </body>
     
+    <p> So much more to learn but in time ill be better <br> 
+    Welcome to the branch test<br> 
+    Lets also put nuxt in the gitignore file</p>
+    </body>
     </div>
 </template>
